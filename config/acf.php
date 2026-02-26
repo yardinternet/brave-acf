e44d6355aab494066949c8c23a938fab48adc5fa
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2026-02-26 10:30:07 +0100

    feat: initial version

---
+++ config/acf.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+	'field_groups' => [
+		//Example::class,
+	],
+	'forms' =>
+	[
+		//Example::class
+	],
+	'option_pages' =>
+	[
+		//Example::class
+	],
+];
